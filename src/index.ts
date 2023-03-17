81aceee20c282a2de65dfe531b20c1c667796ec3
Pukspuu <kaarelkor@gmail.com>
2023-03-17 11:31:26 +0200

    dcvc

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
-import bodyParser from "body-parser";
+
 import express from "express";
+import cors from "cors";
 import pg from "pg";
 
-// Connect to the database using the DATABASE_URL environment
-//   variable injected by Railway
 const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
 
-app.use(bodyParser.json());
-app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
-app.use(bodyParser.text({ type: "text/html" }));
+app.use(cors());
+app.use(express.json());
 
-app.get("/", async (req, res) => {
-  const { rows } = await pool.query("SELECT NOW()");
-  res.send(`Hello, World! The time from the DB is ${rows[0].now}`);
+
+app.get("/treks", async (req, res) => {
+  
+  const columns = req.query.columns;
+  const status = req.query.status;
+
+  let sql = '';
+  let sqlColumns='*';
+  let sqlAdditionalFilters = '';
+
+  if (columns !==undefined){
+   // sqlColumns = columns;
+  }
+  if (status !== undefined){
+    sqlAdditionalFilters +=  `WHERE status = '${status}'`
+  }
+
+  sql = `SELECT ${sqlColumns} FROM treks ${sqlAdditionalFilters};`
+  console.log(sql);
+  const { rows } = await pool.query(sql);
+  res.send(JSON.stringify(rows));
 });
 
 app.listen(port, () => {
